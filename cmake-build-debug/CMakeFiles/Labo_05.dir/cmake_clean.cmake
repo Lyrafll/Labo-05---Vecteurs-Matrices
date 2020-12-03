41a425fdfce989cfdd5e89f8ee9b8015b80ac492
Richard Aurélien <aurelien.r@hotmail.ch>
2020-12-03 13:43:21 +0100

    Init

---
+++ cmake-build-debug/CMakeFiles/Labo_05.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo_05.dir/main.cpp.obj"
+  "Labo_05.exe"
+  "Labo_05.exe.manifest"
+  "Labo_05.pdb"
+  "libLabo_05.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo_05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
